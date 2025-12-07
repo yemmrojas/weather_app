c3cb300d9cfdf3aec85d7c692ff806fad958cbd8
yeimerrojas <yeimerrojasb@hotmail.com>
2025-12-06 22:20:48 -0500

    feat: Add splash screen and basic UI structure
    This commit introduces the splash screen and sets up the initial UI structure for the weather search feature.
    
    - **Splash Screen**:
        - Implements `SplashScreen.kt`, a new Composable that displays the app logo and name.
        - Adds `ic_weather.xml`, a vector drawable for the app icon.
        - Updates `MainActivity.kt` to show the `SplashScreen` on startup, removing the placeholder "Hello Android" text.
        - Adds new color definitions (`Purple700`) to `Color.kt` for the splash screen design.
    - **Search Screen**:
        - Creates a placeholder `WeatherSearchScreen.kt` for the future weather search functionality.
    - **Changelog**:
        - Updates `CHANGELOG.md` to record the implementation of the presentation screen.

---
+++ app/src/main/java/com/yei/dev/weather_app/presentation/search/WeatherSearchScreen.kt
@@ -0,0 +1,12 @@
+package com.yei.dev.weather_app.presentation.search
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WeatherSearchScreen() {
+}
+
+@Composable
+private fun ContentScreen() {
+
+}

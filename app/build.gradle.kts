ae44ca2dea0dcc161ebd43a6e26cbad3458c4dfe
yeimerrojas <yeimerrojasb@hotmail.com>
2025-12-08 11:14:23 -0500

    test: Add testing libraries and remove default test file
    This commit sets up the project with essential libraries for unit and instrumentation testing, and removes the default example unit test.
    
    - **Dependencies Added**:
        - `mockk`: For mocking in unit and instrumentation tests.
        - `kotlinx-coroutines-test`: For testing coroutines.
        - `turbine`: To facilitate testing Kotlin Flows.
    - **Project Configuration**:
        - `libs.versions.toml`: Updated with versions for `mockk`, `coroutinesTest`, and `turbine`.
        - `app/build.gradle.kts`: Includes the new testing dependencies in `testImplementation` and `androidTestImplementation` configurations.
    - **Cleanup**:
        - Deletes `ExampleUnitTest.kt` as it is no longer needed.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,8 +84,13 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
+    // Testing
     testImplementation(libs.junit)
+    testImplementation(libs.mockk)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.turbine)
     androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.mockk.android)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
